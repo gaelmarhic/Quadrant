86c5877feb2b26a7274b29c1397e202bf231bd8b
Gaël Marhic <gael.marhic.pro@gmail.com>
2020-01-01 19:52:04 +0100

    Create QuadrantPlugin class

---
+++ src/main/kotlin/com/gaelmarhic/quadrant/QuadrantPlugin.kt
@@ -0,0 +1,11 @@
+package com.gaelmarhic.quadrant
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class QuadrantPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
