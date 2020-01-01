34b8b8af1b2400390c27ffde55108b2fc7ccf6e2
Gaël Marhic <gael.marhic.pro@gmail.com>
2020-01-01 18:55:43 +0100

    Create project

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,8 @@
+object Dependencies {
+
+    // Plugin
+    const val kotlinPoet = "com.squareup:kotlinpoet:${Versions.kotlinPoet}"
+
+    // Testing
+    const val jUnit = "junit:junit:${Versions.jUnit}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,8 @@
+object Versions {
+
+    // Plugin
+    const val kotlinPoet = "1.4.4"
+
+    // Testing
+    const val jUnit = "4.13"
+}
