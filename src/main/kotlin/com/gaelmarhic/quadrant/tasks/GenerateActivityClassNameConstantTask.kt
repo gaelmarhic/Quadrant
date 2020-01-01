83b75142e073b016eada74603e3f956f0f58fc43
Gaël Marhic <gael.marhic.pro@gmail.com>
2020-01-01 19:56:04 +0100

    Create GenerateActivityClassNameConstantTask class

---
+++ src/main/kotlin/com/gaelmarhic/quadrant/tasks/GenerateActivityClassNameConstantTask.kt
@@ -0,0 +1,12 @@
+package com.gaelmarhic.quadrant.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class GenerateActivityClassNameConstantTask : DefaultTask() {
+
+    @TaskAction
+    fun generateConstants() {
+        // TODO: To be implemented.
+    }
+}
