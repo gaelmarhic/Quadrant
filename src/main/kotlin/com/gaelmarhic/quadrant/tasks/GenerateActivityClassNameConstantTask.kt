c40802759ad49bbe2fb6e7d959708337bc4af2dc
Gaël Marhic <gael.marhic.pro@gmail.com>
2020-01-02 17:15:37 +0100

    Add group and description to GenerateActivityClassNameConstantTask

--- src/main/kotlin/com/gaelmarhic/quadrant/tasks/GenerateActivityClassNameConstantTask.kt
+++ src/main/kotlin/com/gaelmarhic/quadrant/tasks/GenerateActivityClassNameConstantTask.kt
@@ -1,12 +1,23 @@
 package com.gaelmarhic.quadrant.tasks
 
+import com.gaelmarhic.quadrant.constants.QuadrantConstants.PLUGIN_NAME
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class GenerateActivityClassNameConstantTask : DefaultTask() {
 
+    init {
+        group = PLUGIN_NAME
+        description = DESCRIPTION
+    }
+
     @TaskAction
     fun generateConstants() {
         // TODO: To be implemented.
+    }
+
+    companion object {
+
+        private const val DESCRIPTION = "Generates files of constants that hold the Android Activities' full class name."
     }
 }
