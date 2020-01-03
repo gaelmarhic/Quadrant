4bd0d74e236a7dd0cf4c0aa8ff27be8b7b5162b3
Gaël Marhic <gael.marhic.pro@gmail.com>
2020-01-03 19:53:59 +0100

    Create ConstantFileDeterminationHelper

---
+++ src/main/kotlin/com/gaelmarhic/quadrant/helpers/ConstantFileDeterminationHelper.kt
@@ -0,0 +1,44 @@
+package com.gaelmarhic.quadrant.helpers
+
+import com.gaelmarhic.quadrant.constants.GeneralConstants.PLUGIN_NAME
+import com.gaelmarhic.quadrant.models.generation.ConstantToBeGenerated
+import com.gaelmarhic.quadrant.models.generation.FileToBeGenerated
+import com.gaelmarhic.quadrant.models.manifest.Activity
+import com.gaelmarhic.quadrant.models.modules.FilteredModule
+
+class ConstantFileDeterminationHelper {
+
+    fun determine(filteredModules: List<FilteredModule>) =
+        filteredModules
+            .toSingleFile()
+
+    private fun List<FilteredModule>.toSingleFile() = listOf(
+        FileToBeGenerated(
+            name = SINGLE_FILE_NAME,
+            constantList = this
+                .flatMap { it.activityList }
+                .map { it.toConstant() }
+        )
+    )
+
+    private fun Activity.toConstant() = ConstantToBeGenerated(
+        name = name.formatConstantName(),
+        value = name
+    )
+
+    private fun String.formatConstantName() =
+        split(PACKAGE_SEPARATOR)
+            .last()
+            .mapIndexed { index, letter -> formatLetter(index, letter) }
+            .joinToString(EMPTY_SEPARATOR)
+
+    private fun formatLetter(index: Int, letter: Char) =
+        if (letter.isUpperCase() && index != 0) "_$letter" else letter.toUpperCase().toString()
+
+    companion object {
+
+        private const val SINGLE_FILE_NAME = "${PLUGIN_NAME}Constants"
+        private const val PACKAGE_SEPARATOR = "."
+        private const val EMPTY_SEPARATOR = ""
+    }
+}
