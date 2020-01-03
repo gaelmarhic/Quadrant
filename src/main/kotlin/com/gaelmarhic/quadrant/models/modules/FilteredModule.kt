e86e1a008a705fc9b1992c1b1827713d198d50c3
Gaël Marhic <gael.marhic.pro@gmail.com>
2020-01-03 19:52:56 +0100

    Create FilteredModule

---
+++ src/main/kotlin/com/gaelmarhic/quadrant/models/modules/FilteredModule.kt
@@ -0,0 +1,8 @@
+package com.gaelmarhic.quadrant.models.modules
+
+import com.gaelmarhic.quadrant.models.manifest.Activity
+
+data class FilteredModule(
+    val name: String,
+    val activityList: List<Activity>
+)
