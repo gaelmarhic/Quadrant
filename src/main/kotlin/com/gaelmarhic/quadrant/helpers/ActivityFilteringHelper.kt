0144f82bec26a35fa689bfe48f645bce141e1679
Gaël Marhic <gael.marhic.pro@gmail.com>
2020-01-03 20:35:52 +0100

    Create ActivityFilteringHelper

---
+++ src/main/kotlin/com/gaelmarhic/quadrant/helpers/ActivityFilteringHelper.kt
@@ -0,0 +1,24 @@
+package com.gaelmarhic.quadrant.helpers
+
+import com.gaelmarhic.quadrant.models.manifest.Activity
+import com.gaelmarhic.quadrant.models.manifest.Manifest
+import com.gaelmarhic.quadrant.models.modules.FilteredModule
+import com.gaelmarhic.quadrant.models.modules.ParsedModule
+
+class ActivityFilteringHelper {
+
+    fun filter(parsedModules: List<ParsedModule>) =
+        parsedModules
+            .map { module -> module.filterActivities() }
+
+    private fun ParsedModule.filterActivities() =
+        applyFilter { noFilter }
+
+    private fun ParsedModule.applyFilter(predicate: List<Manifest>.() -> List<Activity>) = FilteredModule(
+        name = name,
+        filteredActivityList = predicate(manifestList)
+    )
+
+    private val List<Manifest>.noFilter
+        get() = this.flatMap { it.application.activityList }
+}
