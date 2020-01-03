60f11c19ad115989d41c59cb9b493aabc2f8823d
Gaël Marhic <gael.marhic.pro@gmail.com>
2020-01-03 13:27:20 +0100

    Create MetaData model

--- src/main/kotlin/com/gaelmarhic/quadrant/models/Constants.kt
+++ src/main/kotlin/com/gaelmarhic/quadrant/models/Constants.kt
@@ -6,5 +6,8 @@ object Constants {
     const val APPLICATION_TAG = "application"
     const val ACTIVITY_TAG = "activity"
     const val ACTIVITY_NAME_ATTRIBUTE = "name"
+    const val METADATA_TAG = "meta-data"
+    const val METADATA_NAME_ATTRIBUTE = "name"
+    const val METADATA_VALUE_ATTRIBUTE = "value"
     const val ANDROID_NAMESPACE = "http://schemas.android.com/apk/res/android"
 }
---
+++ src/main/kotlin/com/gaelmarhic/quadrant/models/MetaData.kt
@@ -0,0 +1,22 @@
+package com.gaelmarhic.quadrant.models
+
+import com.gaelmarhic.quadrant.models.Constants.ANDROID_NAMESPACE
+import com.gaelmarhic.quadrant.models.Constants.METADATA_NAME_ATTRIBUTE
+import com.gaelmarhic.quadrant.models.Constants.METADATA_TAG
+import com.gaelmarhic.quadrant.models.Constants.METADATA_VALUE_ATTRIBUTE
+import javax.xml.bind.annotation.XmlAccessType.FIELD
+import javax.xml.bind.annotation.XmlAccessorType
+import javax.xml.bind.annotation.XmlAttribute
+import javax.xml.bind.annotation.XmlRootElement
+
+@XmlRootElement(name = METADATA_TAG)
+@XmlAccessorType(FIELD)
+data class MetaData(
+    @field:XmlAttribute(name = METADATA_NAME_ATTRIBUTE, namespace = ANDROID_NAMESPACE)
+    val name: String,
+    @field:XmlAttribute(name = METADATA_VALUE_ATTRIBUTE, namespace = ANDROID_NAMESPACE)
+    val value: String
+) {
+    @Suppress("unused")
+    constructor() : this(name = "", value = "")
+}
