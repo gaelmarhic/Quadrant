255032b32148b3a18f4aaab872bd77720df4b5a2
Gaël Marhic <gael.marhic.pro@gmail.com>
2020-01-04 17:06:00 +0100

    Implement GenerateActivityClassNameConstants

--- src/main/kotlin/com/gaelmarhic/quadrant/tasks/GenerateActivityClassNameConstants.kt
+++ src/main/kotlin/com/gaelmarhic/quadrant/tasks/GenerateActivityClassNameConstants.kt
@@ -2,13 +2,34 @@ package com.gaelmarhic.quadrant.tasks
 
 import com.gaelmarhic.quadrant.constants.GeneralConstants.PLUGIN_CONFIG
 import com.gaelmarhic.quadrant.constants.GeneralConstants.PLUGIN_NAME
+import com.gaelmarhic.quadrant.constants.GeneralConstants.TARGET_DIRECTORY
 import com.gaelmarhic.quadrant.extensions.QuadrantConfigurationExtension
+import com.gaelmarhic.quadrant.helpers.ActivityFilteringHelper
+import com.gaelmarhic.quadrant.helpers.ConstantFileDeterminationHelper
+import com.gaelmarhic.quadrant.helpers.ConstantGenerationHelper
+import com.gaelmarhic.quadrant.helpers.ManifestParsingHelper
+import com.gaelmarhic.quadrant.models.modules.RawModule
+import com.gaelmarhic.quadrant.processors.GenerateActivityClassNameConstantProcessor
 import org.gradle.api.DefaultTask
+import org.gradle.api.Project
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 
 open class GenerateActivityClassNameConstants : DefaultTask() {
 
     private val configurationExtension by lazy { retrieveConfigurationExtension() }
+
+    private val rawModules by lazy { retrieveRawModules() }
+
+    private val processor by lazy { initProcessor() }
+
+    @InputFiles
+    val manifestFiles = rawModules?.flatMap { it.manifestFiles }
+
+    @OutputDirectory
+    val targetDirectory = project.buildDir.resolve(TARGET_DIRECTORY)
 
     init {
         group = PLUGIN_NAME
@@ -17,14 +38,41 @@ open class GenerateActivityClassNameConstants : DefaultTask() {
 
     @TaskAction
     fun generateConstants() {
-        // TODO: To be implemented.
+        rawModules?.let { processor.process(it) }
     }
 
     private fun retrieveConfigurationExtension() =
         project.extensions.findByName(PLUGIN_CONFIG) as QuadrantConfigurationExtension
 
+    private fun retrieveRawModules() =
+        project // This project is the project of the module where the plugin is applied.
+            .parent
+            ?.subprojects
+            ?.map { it.toRawModule() }
+
+    private fun Project.toRawModule() = RawModule(
+        name = name,
+        manifestFiles = manifestFiles
+    )
+
+    private val Project.manifestFiles: List<File>
+        get() = projectDir
+            .walk()
+            .maxDepth(MANIFEST_FILE_DEPTH)
+            .filter { it.name == MANIFEST_FILE_NAME }
+            .toList()
+
+    private fun initProcessor() = GenerateActivityClassNameConstantProcessor(
+        manifestParsingHelper = ManifestParsingHelper(),
+        activityFilteringHelper = ActivityFilteringHelper(),
+        constantFileDeterminationHelper = ConstantFileDeterminationHelper(),
+        constantGenerationHelper = ConstantGenerationHelper(targetDirectory)
+    )
+
     companion object {
 
+        private const val MANIFEST_FILE_DEPTH = 3
+        private const val MANIFEST_FILE_NAME = "AndroidManifest.xml"
         private const val DESCRIPTION =
             "Generates files of constants that hold the Android Activities' full class name."
     }
